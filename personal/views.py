2fa381b1dcdbc02928582f72a7d807a7788bbee9
Vamsi <s.vamsirao@gmail.com>
2017-08-08 16:06:59 +0530

    Create contact page

--- personal/views.py
+++ personal/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'personal/home.html')
+
+def contact(request):
+	return render(request,'personal/basic.html',{'contact':['If you would like to contact me, please drop a mail','s.vamsirao@gmail.com']})
